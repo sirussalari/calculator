6f5f18ff11fce8f2b30348fcb0de5a9f21166963
sirussalari <sirus.salari@gmail.com>
2022-03-22 17:56:14 -0700

    Add functionality for displaying the numbers clicked and displaying the answer
    The users can now see live feedback on the screen for the digits that they have pressed. Once the user clicks an operator, the display clears and then updates again with the new digits they enter. After they press the equals, the display will then display the resulting number. In addition, the user is able to do new subsequent calculations after each calculation.

--- app.js
+++ app.js
@@ -28,3 +28,62 @@ function operate(x, operator, y) {
         return divide(x, y);
     }
 }
+
+const display = document.querySelector('#display');
+const digits = document.querySelector('#digits').children;
+const operators = document.querySelector('#operators').children;
+let displayValue = '';
+let firstNumber = null;
+let secondNumber = null;
+let operatorChoice = '';
+let reset = false;
+let operatorClicked = false;
+
+for (let digit of digits) {
+    digit.addEventListener('click', () => {
+        if (reset) {
+            displayValue = '';
+            display.textContent = displayValue;
+            reset = false;
+        }
+        displayValue += digit.textContent;
+        display.textContent = displayValue;
+        
+    })
+}
+
+for (let operator of operators) {
+    operator.addEventListener('click', () => {
+        if (!operatorClicked) {
+            firstNumber = parseInt(displayValue);
+            operatorChoice = operator.textContent;
+            displayValue = '';
+            display.textContent = displayValue;
+            if (operator.textContent === '+') {
+                operatorChoice = 'add';
+            }
+            else if (operator.textContent === '-') {
+                operatorChoice = 'subtract';
+            }
+            else if (operator.textContent === '*') {
+                operatorChoice = 'multiply'
+            }
+            else if (operator.textContent === '/') {
+                operatorChoice = 'divide';
+            }
+            operatorClicked = true;
+        }
+        else {
+            if (operator.textContent === '=') {
+                secondNumber = parseInt(displayValue);
+                displayValue = operate(firstNumber, operatorChoice, secondNumber);
+                display.textContent = displayValue;
+                operatorClicked = false;
+                firstNumber = null;
+                operatorChoice = '';
+                secondNumber = null;
+                reset = true;
+            }
+        }
+    })
+}
